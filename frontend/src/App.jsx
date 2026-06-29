e3a095ef4ec810029b160852c359c059833f0bfd
leqsss <leqsss94@gmail.com>
2026-06-29 15:15:29 +0900

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import PrivacyPolicy from './pages/main/PrivacyPolicy.jsx'
 import SchoolPage from './pages/main/SchoolPage.jsx'
 import Login from './pages/main/Login.jsx'
 import ResetPassword from './pages/main/ResetPassword.jsx'
+import StudentHome from './pages/main/StudentHome.jsx'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/schoolpage" element={<SchoolPage />} />
         <Route path="/service" element={<Service />} />
+        <Route path="/studenthome" element={<StudentHome />} />
       </Routes>
     </BrowserRouter>
   )
