3f6038d5d313b48645c5f9f9b7b7e43a531b55ae
leqsss <leqsss94@gmail.com>
2026-06-29 14:42:18 +0900

    feat: initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainPage from './pages/main/MainPage.jsx'
+import Service from './pages/main/Service.jsx'
+import PrivacyPolicy from './pages/main/PrivacyPolicy.jsx'
+import SchoolPage from './pages/main/SchoolPage.jsx'
+import Login from './pages/main/Login.jsx'
+import ResetPassword from './pages/main/ResetPassword.jsx'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/schoolpage" element={<SchoolPage />} />
+        <Route path="/service" element={<Service />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
